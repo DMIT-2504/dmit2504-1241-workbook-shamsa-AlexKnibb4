fa9bcf9a5961a36bf509046415e450e577601657
AlexKnibb4 <157554457+AlexKnibb4@users.noreply.github.com>
2024-09-10 10:22:45 -0600

    Commit Sept 10, 2024 Completed the Student class for the object problem 1

--- week2/object_problem/lib/Student.dart
+++ week2/object_problem/lib/Student.dart
@@ -6,5 +6,47 @@ class Student {
   // constructor
   Student([this._firstName = '' , this._lastName = '', this._idNumber = 0]);
 
-  
+  // getters
+  String get firstName {
+    return _firstName;
+  }
+
+  String get lastName {
+    return _lastName;
+  }
+
+  int get idNumber {
+    return _idNumber;
+  }
+
+  // setters
+  set firstName (String value) {
+    if(value.trim().isEmpty) {
+      throw Exception('First Name cannot be empty.');
+    }
+
+    _firstName = value;
+  }
+
+  set lastName (String value) {
+    if(value.trim().isEmpty) {
+      throw Exception('Last Name cannot be empty.');
+    }
+
+    _lastName = value;
+  }
+
+  set idNumber (int value) {
+    if(value <= 0) {
+      throw Exception('Id Bumber must be positive.');
+    }
+
+    _idNumber = value;
+  }
+
+  // GetFullName method
+  String GetFullName() {
+    return '$_lastName, $_firstName';
+  }
+
 }
